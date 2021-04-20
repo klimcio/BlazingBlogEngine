1eda1c143b94f06da5b862537fb0dba73850a386
Mariusz Klimek <klimcio@gmail.com>
2021-04-20 15:11:01 +0200

    Used Blazor Minimal Template

---
+++ BlazingBlogEngine/IndexBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazingBlogEngine
+{
+    public class IndexBase : LayoutComponentBase
+    {
+        public string GetTextFromMethodInClass()
+        {
+            return "This text is derived from a C# .cs file within the assembly";
+        }
+    }
+}
